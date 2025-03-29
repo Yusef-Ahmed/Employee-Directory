7a3322389488afe556f044e37213d233ab66d796
Youssef Ahmed <yousseffcai@gmail.com>
2025-03-29 14:46:31 +0200

    Feature: home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,33 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 import RootLayout from "./pages/RootLayout";
-import Employees from "./pages/Employees";
-import employeesLoader from "./features/employees/loader";
-import employeesAction from "./features/employees/action";
+import Employees from "./pages/employees/Employees";
+import employeesLoader from "./features/employees/allLoader";
+import employeesAction from "./features/employees/deleteAction";
+import departmentsAction from "./features/departments/deleteAction";
+import jobTitlesAction from "./features/jobTitles/deleteAction";
 import authAction from "./features/authentication/action";
 import NotFound from "./pages/NotFound";
-import Authentication from "./pages/authentication";
+import Authentication from "./pages/Authentication";
+import EditEmployee from "./pages/employees/EditEmployee";
+import editEmployeeLoader from "./features/employees/editLoader";
+import editDepartmentLoader from "./features/departments/editLoader";
+import editJobTitleLoader from "./features/jobTitles/editLoader";
+import departmentsLoader from "./features/departments/allLoader";
+import jobTitlesLoader from "./features/jobTitles/allLoader";
+import editEmployeeAction from "./features/employees/editAction";
+import editDepartmentAction from "./features/departments/editAction";
+import editJobTitleAction from "./features/jobTitles/editAction";
+import addEmployeeAction from "./features/employees/addAction";
+import addDepartmentAction from "./features/departments/addAction";
+import addJobTitleAction from "./features/jobTitles/addAction";
+import AddEmployee from "./pages/employees/AddEmployee";
+import Department from "./pages/departments/Departments";
+import JobTitle from "./pages/jobTitles/JobTitles";
+import AddDepartment from "./pages/departments/AddDepartment";
+import AddJobTitle from "./pages/jobTitles/AddJobTitle";
+import EditDepartment from "./pages/departments/EditDepartment";
+import EditJobTitle from "./pages/jobTitles/EditJobTitle";
 
 function App() {
   const router = createBrowserRouter([
@@ -21,15 +42,75 @@ function App() {
         },
         {
           path: "/employees",
-          element: <Employees />,
-          loader: employeesLoader,
-          action: employeesAction
+          children: [
+            {
+              index: true,
+              element: <Employees />,
+              loader: employeesLoader,
+              action: employeesAction,
+            },
+            {
+              path: ":id",
+              element: <EditEmployee />,
+              loader: editEmployeeLoader,
+              action: editEmployeeAction,
+            },
+            {
+              path: "add",
+              element: <AddEmployee />,
+              action: addEmployeeAction,
+            },
+          ],
+        },
+        {
+          path: "/departments",
+          children: [
+            {
+              index: true,
+              element: <Department />,
+              loader: departmentsLoader,
+              action: departmentsAction,
+            },
+            {
+              path: ":id",
+              element: <EditDepartment />,
+              loader: editDepartmentLoader,
+              action: editDepartmentAction,
+            },
+            {
+              path: "add",
+              element: <AddDepartment />,
+              action: addDepartmentAction,
+            },
+          ],
+        },
+        {
+          path: "/jobTitles",
+          children: [
+            {
+              index: true,
+              element: <JobTitle />,
+              loader: jobTitlesLoader,
+              action: jobTitlesAction,
+            },
+            {
+              path: ":id",
+              element: <EditJobTitle />,
+              loader: editJobTitleLoader,
+              action: editJobTitleAction,
+            },
+            {
+              path: "add",
+              element: <AddJobTitle />,
+              action: addJobTitleAction,
+            },
+          ],
         },
         {
           path: "/auth/:mode",
           element: <Authentication />,
           action: authAction,
-        }
+        },
       ],
     },
   ]);
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,5 +1,34 @@
-function Home () {
-  return <h1 className="text-3xl mt-[50vh] ml-[100vh]">Home</h1>
+import { Link } from "react-router-dom";
+import homeImg from "../assets/homeImg.png";
+import { useScreenSize } from "../components/useScreenSize";
+
+function Home() {
+  const isMobile = useScreenSize();
+
+  return (
+    <main className="flex justify-between items-center h-[80vh]">
+      <section className="lg:w-1/2 lg:ml-7 lg:px-0 lg:items-baseline px-3 mt-5 flex flex-col items-center">
+        <h1 className="font-bold lg:text-6xl lg:text-left text-center text-2xl leading-snug">
+          We Transform{" "}
+          <span className="text-purple-600">Employee Directory</span> into
+          Reality . . .
+        </h1>
+        <p className="mt-5 mb-15 text-gray-400">
+          We transform employee directory management into reality with a
+          seamless, efficient, and user-friendly platform. Easily search,
+          update, and organize employee details in one centralized system,
+          enhancing collaboration and streamlining HR processes.
+        </p>
+        <Link
+          className="bg-indigo-600 rounded-md p-3 ml-2 px-7 hover:bg-indigo-500"
+          to="/employees"
+        >
+          Take a look
+        </Link>
+      </section>
+      {!isMobile && <img className="h-6/7" src={homeImg}></img>}
+    </main>
+  );
 }
 
 export default Home;
